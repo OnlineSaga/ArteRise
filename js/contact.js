31bbcfc9aff6a33557f66b80e3e0d71b5d51d63f
OnlineSaga <onlinesaga.net@gmail.com>
2023-02-24 11:17:03 +0500

    Made thissssssss

---
+++ js/contact.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+
+
+export default function Contact() {
+  return (
+    <>
+    
+    <section>
+        <div class="handler">
+            {/* <div>
+                We'd love to hear from you
+            </div> */}
+            <div class="head">
+                <h1>Contact Us</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, repudiandae!</p>
+            </div>
+            <div class="content">
+                <div class="entries">
+                    <input type="text" class="entry" name="fullname" id="fullname" placeholder="Full name" />
+                    <input type="text" class="entry" name="fullname" id="fullname" placeholder="Email" />
+                    <input type="text" class="entry" name="fullname" id="fullname" placeholder="Phone" />
+                    <textarea name="querry" id="querry" cols="30" rows="10" class="entry" placeholder="querry"></textarea>
+                </div>
+
+                <div class="info">
+                    <div >
+                        <p><strong>Contact Information</strong></p>
+                        <p>call us: +1 174414</p>
+                        <p>we are open from Mon-Fri</p>
+                        <p>9:00 AM - 10:00 PM</p>
+                    </div>
+                    <div >
+                        <p><strong>Address</strong></p>
+                        <p>Saqib Mamu street</p>
+                        <p>Near Aisha manzil</p>
+                        <p>Nani house, karachi</p>
+                    </div>
+                    <div >
+                        <p><strong>Follow us</strong></p>
+                        <div class="links">
+                            <p>f</p>
+                            <p>in</p>
+                            <p>tw</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="head">
+               <h1>Order</h1>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, repudiandae!</p>
+            </div>
+            <div class="content">
+                <div class="order">
+                    My self <input type="text" class="entry o-input" size='15' placeholder="e.g Qasim" required/>, I work for <input type="text" class="entry o-input" size="15" placeholder="e.g Online Saga"/>,Subseqently wants a <input type="text" class="entry o-input" size="25" placeholder="e.g EPI Management system" required/> for my <input type="text" class="entry o-input" size="20" placeholder="e.g Business/Organization"/>. I have read all of the policies of Online Saga, wanted this thing done. The price will be decided to you via call and other futher supplements and credeintails will be obtained through your Email. Your Email address is <input type="email" class="entry o-input" size="25" placeholder="e.g Qasim@gmail.com" required/> and for further querries contact us (fill the above form) and don't forget to leave the textimonial. Be with us to get Vouchers and special discount & offers. Stay in touch with us. Have a great day!
+                </div>
+            </div>
+            <div class="head">
+                <h1>Textimonial</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, repudiandae!</p>
+             </div>
+             <div class="content">
+                <div>
+                    <textarea name="" id=""></textarea>
+                </div>
+             </div>
+        </div>
+    </section>
+    </>
+  )
+}
