79a8ffffdea6a0977538f5756d4e5aee5c15d584
OnlineSaga <onlinesaga.net@gmail.com>
2023-02-25 16:44:08 +0500

    WOrkeeeeeeddddddddddddd!!!!!!!!!!!!!!!!!!!!

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+const bigCursor = document.querySelector(".big-cursor");
+const smallCursor = document.querySelector(".small-cursor");
+
+
+// Update the position of the div element with a little delay on mousemove and scroll events
+document.addEventListener("mousemove", (e) => {
+    updateCursorPos(e.clientX, e.clientY);
+});
+
+
+document.addEventListener("scroll", () => {
+    updateCursorPos(cursorX, cursorY);
+});
+
+// Helper function to update the position of the cursor element
+let cursorX = 0;
+let cursorY = 0;
+
+function updateCursorPos(x, y) {
+    cursorX = x;
+    cursorY = y;
+    const scrollX = window.scrollX || window.pageXOffset;
+    const scrollY = window.scrollY || window.pageYOffset;
+    smallCursor.style.left = cursorX + scrollX + "px";
+    smallCursor.style.top = cursorY + scrollY + "px";
+    setTimeout(() => {
+        bigCursor.style.left = cursorX + scrollX + "px";
+        bigCursor.style.top = cursorY + scrollY + "px"
+    }, 100); // add a delay of 100ms
+}
+
+// Hide the overlay once the page has loaded
+window.addEventListener('DOMContentLoaded', () => {
+    const overlay = document.querySelector('.overlay');
+    const overlayLogo = document.querySelector('.overlay-logo');
+    const overlayHead = document.querySelector('.overlay-head');
+    overlay.style.display = 'flex';
+    overlayLogo.style.opacity = '1';
+    overlayHead.style.opacity = '1';
+
+    // Hide the overlay after a delay
+    setTimeout(() => {
+        overlay.style.opacity = '0';
+        // overlay.style.display = 'none';
+    }, 2500);
+});
